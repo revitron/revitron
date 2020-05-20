7c10b161cea66143a7cd425ac92a9c43e2d08d80
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-20 09:56:24 +0200

    Update __init__.py

--- revitron/__init__.py
+++ revitron/__init__.py
@@ -1,9 +1,13 @@
 from _element import *
-from _rvt import *
+from _filter import *
+from _parameter import *
 from _selection import *
+import Autodesk.Revit.DB
 
 DOC = __revit__.ActiveUIDocument.Document
 UIDOC = __revit__.ActiveUIDocument
+APP = DOC.Application
+DB = Autodesk.Revit.DB
 
 class _(Element):
     pass
