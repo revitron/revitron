9ec01e00eac70592e3cd3855d359decf4d642cbb
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-26 12:20:59 +0200

    Update _element.py

--- revitron/_element/_element.py
+++ revitron/_element/_element.py
@@ -10,7 +10,10 @@ class Element:
     def getClassName(self):
         return self.el.__class__.__name__
     
+    def get(self, paramName):
+        return revitron.Parameter(self.el, paramName).get()
+    
     def set(self, paramName, value, paramType = 'Text'):
         revitron.Parameter.bind(self.el.Category.Name, paramName, paramType)
-        revitron.Parameter.set(self.el, paramName, value)
+        revitron.Parameter(self.el, paramName).set(value)
         return self
