d726f104aa5ee9efa8710f26a867b2448c309213
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-09 15:48:49 +0200

    Create _category.py

---
+++ revitron/_category.py
@@ -0,0 +1,36 @@
+import revitron
+
+class Category:
+    
+    def __init__(self, name):
+        """
+        Init a new Category by name.
+
+        Args:
+            name (string): The category name
+        """
+        self.category = revitron.DOC.Settings.Categories.get_Item(name)
+        
+    def get(self):
+        """
+        Returns the category object.
+
+        Returns:
+            object: The category object
+        """
+        return self.category
+    
+    def getBic(self):
+        """
+        Returns the built-in category for a given category.
+
+        Returns:
+            object: The built-in category
+        """        
+        for item in dir(revitron.DB.BuiltInCategory):
+            try:
+                bic = getattr(revitron.DB.BuiltInCategory, item)
+                if int(bic) == self.category.Id.IntegerValue:
+                    return bic
+            except:
+                pass  
