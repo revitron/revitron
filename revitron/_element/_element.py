35c7817366d489aa2110f1945ad8b932d9cb686e
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-09 18:29:16 +0200

    Update _element.py

--- revitron/_element/_element.py
+++ revitron/_element/_element.py
@@ -2,18 +2,48 @@ import revitron
 
 class Element:
     
-    el = None
-    
     def __init__(self, element):
+        """
+        Inits a new element instance.
+
+        Args:
+            element (object): The Revit element
+        """        
         self.element = element
     
     def getClassName(self):
+        """
+        Returns the class name of the element.
+
+        Returns:
+            string: The class name
+        """        
         return self.element.__class__.__name__
     
     def get(self, paramName):
+        """
+        Returns a parameter value.add()
+
+        Args:
+            paramName (string): The name of the parameter
+
+        Returns:
+            mixed: The parameter value
+        """        
         return revitron.Parameter(self.element, paramName).get()
     
     def set(self, paramName, value, paramType = 'Text'):
+        """
+        Sets a parameter value.add()
+
+        Args:
+            paramName (string): The parameter name
+            value (mixed): The value
+            paramType (string, optional): The parameter type. Defaults to 'Text'.
+
+        Returns:
+            object: The element instance
+        """        
         revitron.Parameter.bind(self.element.Category.Name, paramName, paramType)
         revitron.Parameter(self.element, paramName).set(value)
         return self
