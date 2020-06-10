43b19681ad4ae5dc5189f1af82aa810df8e9bafd
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-10 17:43:27 +0200

    Create _externalReference.py

---
+++ revitron/_externalReference.py
@@ -0,0 +1,14 @@
+import revitron
+
+class ExternalReference:
+    
+    def __init__(self, ref):
+        """
+        Inits a new ExternalReference instance.
+
+        Args:
+            ref (object): A Revit external reference instance.
+        """        
+        self.ref = ref
+        self.type = ref.ExternalFileReferenceType
+        self.path = revitron.DB.ModelPathUtils.ConvertModelPathToUserVisiblePath(ref.GetAbsolutePath())
