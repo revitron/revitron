95531966888a206bd263c2db1140770e74de462e
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-11 19:38:27 +0200

    Update selection.py

--- revitron/selection.py
+++ revitron/selection.py
@@ -5,8 +5,20 @@ class Selection:
     
     @staticmethod
     def get():
+        """
+        Get the currently selected elements.
+
+        Returns:
+            list: The list of selected elements
+        """        
         return [revitron.DOC.GetElement(elId) for elId in revitron.UIDOC.Selection.GetElementIds()]
     
     @staticmethod
     def first():
+        """
+        Get the first elements of the list of selected elements.
+
+        Returns:
+            object: The first element in a list of selected elements
+        """
         return Selection.get()[0]
