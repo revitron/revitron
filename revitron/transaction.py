03cc960efab10f402cd2b0a65357e51ab7a6e7e1
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-11 19:38:30 +0200

    Update transaction.py

--- revitron/transaction.py
+++ revitron/transaction.py
@@ -5,10 +5,16 @@ from pyrevit import script
 class Transaction:
     
     def __init__(self):
+        """
+        Inits a new transaction.
+        """        
         bundle = script.get_bundle_name().replace('.pushbutton', '')
         self.transaction = revitron.DB.Transaction(revitron.DOC, bundle)
         self.transaction.Start()
         
     def commit(self):
+        """
+        Commits the open transaction.
+        """        
         self.transaction.Commit()
     
