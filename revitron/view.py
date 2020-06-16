b66e92ef0062afe7f65ad8e2c0970ae6120ad649
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-16 16:28:25 +0200

    Update view.py

--- revitron/view.py
+++ revitron/view.py
@@ -1,14 +1,18 @@
 import revitron
 
-class ViewList:
+class ViewSheetList:
+    """
+    The ViewSheetList class creates a list of custom view objects based on a list of sheets. 
+    """    
     
-    views = []
-    
-    def __init__(self):
-        pass
-    
-    def fromSheets(self, sheets):
-        
+    def __init__(self, sheets):
+        """
+        Inits an new ViewSheetList object. 
+
+        Args:
+            sheets (list): A list with sheets
+        """      
+        views = []  
         for sheet in sheets:
             if revitron.Element(sheet).getClassName() == 'ViewSheet':
                 for viewId in sheet.GetAllPlacedViews():
@@ -17,8 +21,23 @@ class ViewList:
                     item.sheet = sheet
                     item.view = revitron.DOC.GetElement(viewId)
                     self.views.append(item)
-                    
-        return self
-                    
+    
+    
     def get(self):
+        """
+        Returns the list of view objects. The view objects have the following properties:
+
+        - `id`: The ID of the view
+        - `sheet`: The sheet object where the view is placed on
+        - `view`: The actual view object
+        
+        The returned list can be used as follows::
+        
+            sheets = revitron.Filter().byCategory('Sheets').getElements()
+            for view in revitron.ViewSheetList(sheets).get():
+                print(view.id, view.sheet, view.view)
+
+        Returns:
+            list: A list with view objects as described above
+        """        
         return self.views
