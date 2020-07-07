0042add5d17287fcfc73d3a7d7efa84b2d64cda0
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-07-07 15:30:14 +0200

    Create export.py

---
+++ revitron/export.py
@@ -0,0 +1,152 @@
+#-*- coding: UTF-8 -*-
+import revitron
+import os, shutil, time, sys, glob, re
+from pyrevit import script
+
+
+class PDFExporter:
+    """
+    Export sheets to PDF named by a file naming template. 
+    """
+    
+    def __init__(self, printer, output):
+        """
+        Inits a new PDFExporter instance.
+
+        Args:
+            printer (string): The printer network adress
+            output (string): The printer output directory 
+        """
+        self.printer = printer
+        self.output = output
+        self.manager = revitron.DOC.PrintManager
+        self.sizes = dict()
+        
+        if self.manager.PrinterName.lower() != self.printer.lower():
+            print('Setting current printer to: ' + self.printer)
+            print('Please submit your sheets to be exported again ...')
+            self.manager.SelectNewPrintDriver(self.printer)
+            self.manager.Apply()
+            sys.exit()
+            
+        self.manager.PrintRange = revitron.DB.PrintRange.Select    
+        self.manager.PrintToFile = True
+        self.manager.CombinedFile = False
+        self.manager.Apply()
+        
+        for size in self.manager.PaperSizes:
+            self.sizes[size.Name] = size
+        
+        
+    def printSheet(self, sheet, size, orientation = 'Landscape', directory = False, template = '{Sheet Number}-{Sheet Name}'):
+        """
+        Prints a sheet.
+
+        Args:
+            sheet (object): A Revit sheet
+            size (string): A size name like A0 or A4
+            orientation (string, optional): The orientation, 'Landscape' or 'Portrait'. Defaults to 'Landscape'.
+            directory (string, optional): A custom output directory. Defaults to False.
+            template (string, optional): A name template. Defaults to '{Sheet Number}-{Sheet Name}'.
+
+        Returns:
+            bool: False on error
+        """
+        if revitron.Element(sheet).getClassName() != 'ViewSheet':
+            print(':face_with_rolling_eyes: Element is not a sheet!')
+            return False
+
+        if not directory:
+            directory = self.output
+  
+        path = directory + os.sep + revitron.ParameterTemplate(sheet, template).get() + '.pdf'	
+        transaction = revitron.Transaction()
+        
+        viewSet = revitron.DB.ViewSet()
+        viewSet.Insert(sheet)
+  
+        viewSheetSetting = self.manager.ViewSheetSetting
+        viewSheetSetting.CurrentViewSheetSet.Views = viewSet
+        viewSheetSetting.SaveAs("_temp_")
+  
+        self.manager.PrintSetup.SaveAs("_temp_")
+        self.manager.Apply()
+
+        orientation = getattr(revitron.DB.PageOrientationType, orientation)
+  
+        # Set current print page settings.
+        printParameters = self.manager.PrintSetup.CurrentPrintSetting.PrintParameters
+        printParameters.ZoomType = revitron.DB.ZoomType.Zoom
+        printParameters.Zoom = 100
+        printParameters.PaperPlacement = revitron.DB.PaperPlacementType.Center
+        printParameters.PageOrientation = orientation
+        printParameters.PaperSize = self.sizes[size]
+        printParameters.RasterQuality = revitron.DB.RasterQualityType.High
+
+        # Set in-session print settings.
+        printParameters = self.manager.PrintSetup.InSession.PrintParameters
+        printParameters.ZoomType = revitron.DB.ZoomType.Zoom
+        printParameters.Zoom = 100
+        printParameters.PaperPlacement = revitron.DB.PaperPlacementType.Center
+        printParameters.PageOrientation = orientation
+        printParameters.PaperSize = self.sizes[size]
+        printParameters.RasterQuality = revitron.DB.RasterQualityType.High
+  
+        # Again save settings.
+        try:
+            self.manager.PrintSetup.Save()
+        except:
+            self.manager.PrintSetup.SaveAs("_temp2_")
+        
+        self.manager.Apply()
+        self.manager.SubmitPrint(sheet)
+        viewSheetSetting.Delete()
+  
+        transaction.rollback()
+  
+        # Move file form temp output to directory.
+        timePassed = time.time()
+        moved = False
+        
+        while (time.time() - timePassed) < 30 and not moved:
+            time.sleep(0.5)
+            tempFiles = glob.glob(self.tempOutputPattern(sheet))
+            if tempFiles:
+                tempFile = tempFiles[0]
+                time.sleep(2)
+                if os.access(tempFile, os.W_OK):
+                    try:
+                        shutil.move(tempFile, path)
+                        moved = True
+                    except:
+                        pass
+  
+        if moved:
+            script.get_output().print_html(':smiling_face: Exported <em>{}</em> &mdash; <a href="file:///{}" target="_blank">Open Folder</a>'.format(os.path.basename(path), os.path.dirname(path).replace('\\', '/')))
+        else:
+            script.get_output().print_html(':pouting_face: Error exporting <em>{}</em> '.format(os.path.basename(path)))
+
+        return True
+  
+
+    def tempOutputPattern(self, sheet):
+        """
+        Create a glob pattern to identify a printed PDF in the system output directory to be able to move it to its correct location and rename it according to the given template.
+        Please note that the PDF printer has to be configured to publisg PDFs following the below naming scheme::
+        
+            [Revit File] - Sheet - [Sheet Number] - [Sheet Name].pdf
+        
+        For example::
+        
+            Project1 - Sheet - A101 - Unnamed.pdf
+
+        Args:
+            sheet ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        nr = re.sub(r'[^a-zA-Z0-9]+', '*', revitron.Element(sheet).get('Sheet Number'))
+        name = re.sub(r'[^a-zA-Z0-9]+', '*', revitron.Element(sheet).get('Sheet Name'))
+        rvt = re.sub(r'\.rvt$', '', os.path.basename(revitron.DOC.PathName))
+        return '{}/{}*Sheet*{}*{}*.pdf'.format(self.output, rvt, nr, name)  
