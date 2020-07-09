c465829496d978d7abafe235dde4c9cdf9415880
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-07-09 17:50:45 +0200

    Update docs.bat

--- docs.bat
+++ docs.bat
@@ -1,6 +1,6 @@
 set cwd=%~dp0
 cd %cwd%
 cd docs
-sphinx-apidoc --ext-viewcode --templatedir source/_templates/ -TMf -o source/ ../revitron
+sphinx-apidoc --separate --ext-autodoc --ext-viewcode --templatedir source/_templates/ -Tf -o source/ ../revitron
 call make html
 cd %cwd%
