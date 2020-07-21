79f6e89f9abd816c4eb0ddc01b5daad9ce8992d4
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-07-21 11:29:54 +0200

    Update filter.py

--- revitron/filter.py
+++ revitron/filter.py
@@ -17,32 +17,51 @@ class Filter:
             self.collector = revitron.DB.FilteredElementCollector(revitron.DOC)
     
     
-    def applyParameterFilter(self, rule):
+    def applyParameterFilter(self, rule, invert = False):
         """
-        Filters the collection by a parameter value.
+        Applies a parameter filter.
 
         Args:
-            paramName (string): The parameter name
-            value (mixed): the value
+            rule (object): The filter rule object
+            invert (boolean): Inverts the filter
         """       
-        parameterFilter = revitron.DB.ElementParameterFilter(rule)
+        parameterFilter = revitron.DB.ElementParameterFilter(rule, invert)
         self.collector = self.collector.WherePasses(parameterFilter)
 
 
-    def applyStringFilter(self, paramName, value, evaluator):
+    def applyStringFilter(self, paramName, value, evaluator, invert = False):
         """
-        Apply a string filter.
+        Applies a string filter.
 
         Args:
             paramName (string): The parameter name
             value (mixed): the value
             evaluator (object): The FilterStringRuleEvaluator
+            invert (boolean): Inverts the filter
         """   
-        valueProvider = revitron.ParameterValueProvider(paramName).get()
-        rule = revitron.DB.FilterStringRule(valueProvider, evaluator, value, True)
-        self.applyParameterFilter(rule)   
+        filters = []
     
-    
+        # Since a visible parameter name could match multiple built-in parameters,
+        # we get a list of value providers. 
+        # While iterating that list, the parameter filter is applied each time 
+        # to a fresh element collector that will be later merged or intersected with the others. 
+        for valueProvider in revitron.ParameterValueProviders(paramName).get():
+            rule = revitron.DB.FilterStringRule(valueProvider, evaluator, value, True)
+            _filter = Filter()
+            _filter.collector = revitron.DB.FilteredElementCollector(revitron.DOC, self.collector.ToElementIds())
+            _filter.applyParameterFilter(rule, invert) 
+            filters.append(_filter)
+        
+        self.collector = filters[0].collector
+        
+        if len(filters) > 1:
+            for i in range(1, len(filters)):
+                if not invert:
+                    self.collector.UnionWith(filters[i].collector)
+                else:
+                    self.collector.IntersectWith(filters[i].collector)
+                
+           
     def byCategory(self, name):
         """
         Filters the collection by a category name - not a built-in category.
@@ -68,63 +87,67 @@ class Filter:
         return self
     
     
-    def byStringContains(self, paramName, value):
+    def byStringContains(self, paramName, value, invert = False):
         """
         Filters the collection by a string contained in a parameter.
 
         Args:
             paramName (string): The parameter name
             value (string): The searched string
+            invert (boolean): Inverts the filter
 
         Returns:
             object: The collector
         """
-        self.applyStringFilter(paramName, value, revitron.DB.FilterStringContains())
+        self.applyStringFilter(paramName, value, revitron.DB.FilterStringContains(), invert)
         return self 
     
     
-    def byStringEquals(self, paramName, value):
+    def byStringEquals(self, paramName, value, invert = False):
         """
         Filters the collection by a string that equals a parameter value.
 
         Args:
             paramName (string): The parameter name
             value (string): The searched string
+            invert (boolean): Inverts the filter
 
         Returns:
             object: The collector
         """
-        self.applyStringFilter(paramName, value, revitron.DB.FilterStringEquals())
+        self.applyStringFilter(paramName, value, revitron.DB.FilterStringEquals(), invert)
         return self    
     
     
-    def byStringBeginsWith(self, paramName, value):
+    def byStringBeginsWith(self, paramName, value, invert = False):
         """
         Filters the collection by a string at the beginning of a parameter value.
 
         Args:
             paramName (string): The parameter name
             value (string): The searched string
+            invert (boolean): Inverts the filter
 
         Returns:
             object: The collector
         """
-        self.applyStringFilter(paramName, value, revitron.DB.FilterStringBeginsWith())
+        self.applyStringFilter(paramName, value, revitron.DB.FilterStringBeginsWith(), invert)
         return self 
     
     
-    def byStringEndsWith(self, paramName, value):
+    def byStringEndsWith(self, paramName, value, invert = False):
         """
         Filters the collection by a string at the end of a parameter.
 
         Args:
             paramName (string): The parameter name
             value (string): The searched string
+            invert (boolean): Inverts the filter
 
         Returns:
             object: The collector
         """
-        self.applyStringFilter(paramName, value, revitron.DB.FilterStringEndsWith())
+        self.applyStringFilter(paramName, value, revitron.DB.FilterStringEndsWith(), invert)
         return self 
     
     
@@ -158,4 +181,14 @@ class Filter:
             list: The list of filtered elements
         """        
         return self.collector.ToElements()
+    
+    
+    def getElementIds(self):
+        """
+        Get the collection as element IDs.
+
+        Returns:
+            list: The list of filtered element IDs
+        """        
+        return self.collector.ToElementIds()
   
