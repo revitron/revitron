ab50317c34aea63e221473efd072eb44840fac8d
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-05 14:06:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,7 +95,7 @@ html_context = {
         'menu': [
             {'title': 'Revitron UI', 'url': 'https://revitron-ui.readthedocs.io/'},
             {'title': 'Docs', 'url': 'revitron.html'},
-            {'title': 'RPM', 'url': 'https://github.com/revitron/rpm/blob/master/README.md'},
+            {'title': 'RPM', 'url': 'https://github.com/revitron/rpm#readme'},
         ]
     }
 }
