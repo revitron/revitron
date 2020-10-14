9e334f551cc686929523e8ee45dc8de0f993a3bb
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-14 18:08:04 +0200

    Update transaction.py

--- revitron/transaction.py
+++ revitron/transaction.py
@@ -19,9 +19,7 @@ class Transaction:
         Inits a new transaction.
         """  
         import revitron
-              
-        bundle = script.get_bundle_name().replace('.pushbutton', '')
-        self.transaction = revitron.DB.Transaction(revitron.DOC, bundle)
+        self.transaction = revitron.DB.Transaction(revitron.DOC, script.get_button().get_title())
         self.transaction.Start()
         
         
