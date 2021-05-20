128e5200421fc5d1c103cb93d1006d6617d596c8
Marc Anton Dahmen <revitron@marcdahmen.de>
2021-05-20 15:04:41 +0200

    ci: remove version number from landing page

--- release.sh
+++ release.sh
@@ -61,7 +61,6 @@ echo
 
 # Updating version numbers.
 sed -i "s|REVITRON_VERSION.*|REVITRON_VERSION = '$tag'|g" "./revitron/__init__.py"
-sed -i "s|Version.*|Version $tag|g" "./docs/source/index.rst"
 
 # Commit, merge and tag.
 git add -A && git commit -m "build(release): prepared release $tag"
