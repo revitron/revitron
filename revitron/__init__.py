3b4a6703b52cc811bfad0a7c7b715cd188b80fbe
Marc Anton Dahmen <revitron@marcdahmen.de>
2021-06-08 20:23:30 +0200

    add geometry module

--- revitron/__init__.py
+++ revitron/__init__.py
@@ -95,6 +95,7 @@ from revitron.document import *
 from revitron.externalreference import *
 from revitron.failure import *
 from revitron.filter import *
+from revitron.geometry import *
 from revitron.link import *
 from revitron.parameter import *
 from revitron.raytrace import *
