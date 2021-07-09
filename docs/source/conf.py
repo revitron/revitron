9984e512fa931c34cca52351a4165beb3d454cfd
Marc Anton Dahmen <revitron@marcdahmen.de>
2021-07-09 16:21:53 +0200

    docs: hide private methods

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ add_module_names = False
 
 napoleon_google_docstring = True
 napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = True
+napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = True
