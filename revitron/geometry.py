bb996787cc2e6d9748ee1a78dd7240206d974020
Marc Anton Dahmen <revitron@marcdahmen.de>
2022-05-17 17:43:59 +0200

    feat(geometry): add GeometryUtils class

--- revitron/geometry.py
+++ revitron/geometry.py
@@ -9,6 +9,7 @@ The full syntax without using the shorthand can also be used::
 
 	geometry = revitron.Geometry(element)
 """
+import math
 
 
 class Geometry:
@@ -61,3 +62,76 @@ class Geometry:
 		except:
 			pass
 		return solids
+
+
+class GeometryUtils:
+	"""
+	The GeometryUtils class contains a collection of static utility methods for dealing
+	with geometry related tasks.
+	"""
+
+	@staticmethod
+	def getAngle(center, p1, p2):
+		"""
+		Calculate the angle in degrees between two vectors that share a common center and 
+		are defined two endpoints.
+
+		Args:
+			center (XYZ): A Revit XYZ object that defines the common start point
+			p1 (XYZ): The endpoint of the first vector
+			p2 (XYZ): The endpoint of the second vector
+
+		Returns:
+			float: The calculated angle in degrees
+		"""
+		vector1 = p1 - center
+		vector2 = p2 - center
+		dotProd = vector1.DotProduct(vector2)
+		magnitude1 = p1.DistanceTo(center)
+		magnitude2 = p2.DistanceTo(center)
+		return math.degrees(math.acos(dotProd / (magnitude1 * magnitude2)))
+
+	@staticmethod
+	def getBoundaryPoints(boundary):
+		"""
+		Get a list of points from a given boundary that is usually returned by 
+		methods such as ``GetBoundarySegments``.
+
+		Args:
+			boundary (list): A list of boundary segment lists
+
+		Returns:
+			list: The list of points that define a boundary polygon
+		"""
+		boundaryPoints = []
+		for segmentsList in boundary:
+			for segment in segmentsList:
+				curve = segment.GetCurve()
+				start = curve.GetEndPoint(0)
+				boundaryPoints.append(start)
+		return boundaryPoints
+
+	@staticmethod
+	def polygonContainsPoint2D(polygonPoints, point):
+		"""
+		Check whether a given polygon that is planar to a horizontal surface
+		contains a given point. Note that both, the polygon as well as the point must share common Z values.
+
+		Args:
+			polygonPoints (list): A list of points
+			point (XYZ): The point that is tested
+
+		Returns:
+			boolean: True, if the polygon contains the point
+		"""
+		for p in polygonPoints:
+			if round(p.Z, 3) != round(point.Z, 3):
+				return False
+		prev = polygonPoints[len(polygonPoints) - 1]
+		accumulated = 0
+		for n in range(0, len(polygonPoints)):
+			_p = polygonPoints[n]
+			angle = GeometryUtils.getAngle(point, prev, _p)
+			prev = _p
+			accumulated = accumulated + angle
+		return accumulated > 180
