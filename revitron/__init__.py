c560c11127096faa658a5f1731b688264ccae00f
Marc Anton Dahmen <revitron@marcdahmen.de>
2022-07-14 12:09:01 +0200

    build(release): prepare release 0.3.1

--- revitron/__init__.py
+++ revitron/__init__.py
@@ -134,7 +134,7 @@ except:
 DB = Autodesk.Revit.DB
 LIB_DIR = parent(parent(__file__))
 REVIT_VERSION = pyrevit.HOST_APP.uiapp.Application.VersionNumber
-REVITRON_VERSION = '0.3.0'
+REVITRON_VERSION = '0.3.1'
 
 
 def _(element):
